a7533212005cda3f96fc49dd005fe20899c74d32
moonjeong4 <jungmoon0528@gmail.com>
2023-10-12 21:27:19 -0400

    initial

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import Logo from "./Logo";
+
+function App() {
+  return (
+    <div className="h-screen bg-violet-950 ">
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+function Logo() {
+  return <h1 className="text-center text-2xl text-yellow-300">🌕 List ✅📋</h1>;
+}
+
+export default Logo;
